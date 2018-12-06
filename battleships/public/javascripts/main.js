73ad30ee9a11b8512a2ae1dfa2fe21ec9efbfb57
Almanzo McConkey <almanzomc@gmail.com>
2018-12-06 23:19:34 +0100

    Basic functionality for setting ship positions

--- battleships/public/javascripts/main.js
+++ battleships/public/javascripts/main.js
@@ -3,29 +3,40 @@
 var main = function() {
     "use strict";
 
+    // ships to be placed by the player. Sent to server on ready signal.
     var ships = {
         
-        "5-block-ship": {
+        aircraftCarrier: {
+            name: "Aircraft Carrier",
+            length: 5,
+            unplaced: 1,
+            places: []
+        },
+
+        battleship: {
+            name: "Battleship",
+            length: 4,
             unplaced: 2,
             places: []
         },
 
-        "4-block-ship": {
+        cruiser: {
+            name: "Cruiser",
+            length: 3,
+            unplaced: 1,
+            places: []
+        },
+
+        submarine: {
+            name: "Submarine",
+            length: 3,
             unplaced: 2,
             places: []
         },
 
-        "3-block-ship": {
-            unplaced: 2,
-            places: []
-        },
-
-        "2-block-ship": {
-            unplaced: 2,
-            places: []
-        },
-
-        "1-block-ship": {
+        destroyer: {
+            name: "Destroyer",
+            length: 2,
             unplaced: 2,
             places: []
         }
@@ -41,14 +52,59 @@ var main = function() {
         return true;
     }
 
+    // constructs an array of cells from cell to cell + horizontal steps
+    var cellHorizontalArray = function($cell, steps) {
+        var cells = [];
+
+        for (let i=0; i<steps; i++) {
+            cells.push($cell[0]);
+            $cell = $cell.next();
+        }
+        return cells;
+    }
+
+    // constructs an array of cells from cell to cell + vertical steps
+    var cellVerticalArray = function($cell, steps) {
+        var cells = [];
+
+        var $row = $cell.parent();
+        for (let i=0; i<steps; i++) {
+            cells.push($cell[0]);
+
+            $row = $row.next();
+            $cell = $row.children("[column='" + $cell.attr("column") + "']")
+        }
+
+        return cells;
+    }
+
+    // function to check if one or more cells in an array contain a ship.
+    var containsShip = function(cells) {
+        for (let i=0; i<cells.length; i++) {
+            if ($(cells[i]).hasClass("ship")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // function to set a ship at all cells in the given array.
+    var setShip = function(cells) {
+        cells.forEach( (cell) => {
+            $(cell).addClass("ship");
+        });
+    }
+
     // generate boards made up from divs
     var generateBoard = function(width, height) {
-        var board = $("<div>");
+        var board = $("<div>").addClass("board");
+        board.attr("width", width);
+        board.attr("height", height);
 
         var rowDiv;
         var el;
         for (let row=0; row<height; row++) {
-            rowDiv = $("<div>");
+            rowDiv = $("<div>").addClass("row");
             for(let column=0; column<width; column++) {
                 el = $("<div>").addClass("cell");
                 el.attr("row", row);
@@ -61,34 +117,69 @@ var main = function() {
         return board;
     }
 
-    $(".board").prepend( generateBoard(9, 9) );
+    $(".board-parent").append( generateBoard(10, 10) );
 
     // generate ship radio buttons
-    for(let key in ships) {
+    for (let key in ships) {
         let ship = ships[key];
-        let i = $("<input>").attr("type", "radio").attr("name", "ships").attr("value", true);
-        let l = $("<label>").text(key);
-        l.append( $("<span>").text(" ("+ship.unplaced+") ").addClass("quantity") );
-        l.append(i);
-        $("#ship-pallet").append(l);
+        let input = $("<input>")
+        input.attr("type", "radio").attr("name", "ships").attr("value", key);
+
+        let label = $("<label>").attr("id", key);
+        label.append(input).append(ship.name);
+        label.append(" (<span class=quantity>" + ship.unplaced + "</span>) ");
+
+        $("#ship-pallet").append(label);
     }
 
-    // hide ready button and change status to ready when clicked
+    // add a ship to the player board when the board is clicked
+    $("#player-board .cell").click((event) => {
+        let $cell = $(event.currentTarget);
+        let shipKey = $("#ship-pallet input[name=ships]:checked").val();
+        let rotated = $("#ship-pallet input[name=rotate]:checked").val();
+        let ship = ships[shipKey];
+
+        // ensure ship has been selected and ships of that type are left
+        if (ship && ship.unplaced > 0) {
+
+            // ensure ship fits horizontally or vertically
+            var horizontalFit = !rotated && Number($cell.attr("column")) + ship.length <= $cell.closest(".board").attr("width");
+            var verticalFit = rotated && Number($cell.attr("row")) + ship.length <= $cell.closest(".board").attr("height");
+
+            if (horizontalFit || verticalFit) {
+
+                var cellArray;
+                if (horizontalFit) {
+                    cellArray = cellHorizontalArray($cell, ship.length);
+
+                } else {
+                    cellArray = cellVerticalArray($cell, ship.length);
+                }
+                
+                console.log(containsShip(cellArray));
+                if (!containsShip(cellArray)) {
+                    ship.places.push(cellArray);
+                    setShip(cellArray);
+                    ship.unplaced -= 1;
+                    console.log(shipKey, ship);
+                    console.log("#ship-pallet #" + shipKey + " .quantity");
+                    $("#ship-pallet #" + shipKey + " .quantity").text(ship.unplaced);
+                }
+            }
+        }
+    });
+
+    // hide ready button, turn off clicks for player board and change status to ready when clicked
     $("#ready-button").click((event => {
         if (!shipsPlaced()) {
             window.alert("Please place all of your ships.");
         } else {
             $(event.currentTarget).hide();
             $("#ship-pallet").hide();
+            $("#player-board .cell").off('click');
             $("#message").text("Waiting for opponent to be ready...");
         }
     }));
-
-    // check when the opponent's board is clicked
-    $("#opponent-board .cell").click((event) => {
-        let $cell = $(event.currentTarget);
-        window.alert($cell.attr("row") + ", " + $cell.attr("column"));
-    });
 };
 
 $(document).ready(main);
