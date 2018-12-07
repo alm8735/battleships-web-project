4093bd027b7c276e354c7d23482fa16bcf75c733
Almanzo McConkey <almanzomc@gmail.com>
2018-12-07 21:58:06 +0100

    More websocket stuff
    Installed correct websocket package, did lots of client-server and server-client coding!

--- battleships/app.js
+++ battleships/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const http = require("http");
-const websocket = require("websocket");
+const websocket = require("ws");
+
+///////////////// Data Structures and Helper Functions  ////////////////////////
 
 // function to generate player id (source: https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript)
 function guidGenerator() {
@@ -10,29 +12,97 @@ function guidGenerator() {
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
 
-///////////// Data Structures ///////////////////
+function firstPlayerTurn() {
+  return 0.5 < Math.random();
+}
 
-// list of unpaired players
-unpairedPlayers = {};
-pairedPlayers = [];
+// ships to be placed by the player.
+function ShipType(name, length, quantity) {
+  this.name = name;
+  this.length = length;
+  this.unplaced = quantity;
+  this.places = [];
+}
 
-/////////////////////////////////////////////////
+ function Ships() {
+  this.values = {
+    aircraftCarrier: new ShipType("Aircraft Carrier", 5, 1),
+    battleship: new ShipType("Battleship", 4, 1),
+    cruiser: new ShipType("Cruiser", 3, 2),
+    submarine: new ShipType("Submarine", 3, 1),
+    destroyer: new ShipType("Destroyer", 2, 2)
+   }
+};
 
-// get websocket server interface
-var WebSocketServer = websocket.server;
+// function to check if Ships object is a correct placed version of this one.
+Ships.prototype.validPlacedMatch = function(other) {
+  if (!other) return false;
+  
+  for (let key in this.values) {
+    let thisShip = this.values[key];
+    let otherShip = other[key];
 
-// get port from given arguments
+    if (!otherShip ||
+        otherShip.name !== thisShip.name ||
+        otherShip.length !== thisShip.length ||
+        otherShip.places.length !== thisShip.unplaced ||
+        otherShip.unplaced !== 0) {
+          return false;
+        }
+  }
+
+  return true;
+}
+
+// "class" for a player object.
+function Player(ws){
+  this.ws = ws;
+  this.ready = false;
+  this.turn = false;
+  this.opponent = null;
+  this.ships = new Ships();
+}
+
+// function to send the Ships values to the client.
+Player.prototype.sendShips = function() {
+  var json = JSON.stringify({'ships': this.ships.values});
+  this.ws.send(json);
+}
+
+// function to confirm or deny a ready request
+Player.prototype.respondReadyRequest = function(accepted) {
+  var json = JSON.stringify({'readyAccepted': accepted});
+  this.ws.send(json);
+}
+
+// function to pair player with an opponent
+Player.prototype.pairWith = function(other) {
+  this.opponent = other;
+  other.opponent = this;
+}
+
+// function to send when player is paired with an opponent
+Player.prototype.sendPairedMessage = function() {
+  this.ws.send( JSON.stringify({'foundOpponent': true, 'yourTurn':this.turn}));
+}
+
+// dictionary pairing ids to players
+var players = {};
+
+// variable to store a waiting player
+var waitingPlayer = null;
+
+//////////////////////// Port, HTTP and Express Setup /////////////////////////
+
 var port = process.argv[2];
-
-// create express object
 var app = express();
+
 app.use(express.static(__dirname + "/public"));
+
+var server = http.createServer(app);
 
 // create and open http server
 var server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
-});
 
 // basic express routes
 app.get("/splash", (req, res, next) => {
@@ -43,21 +113,66 @@ app.get("/game", (req, res, next) => {
   res.sendFile("game.html", {root: "./public"});
 });
 
+/////////////////////// WebSocket Initialization /////////////////////
+
 // create WebSocket server using http server
-var wsServer = new WebSocketServer({
-  httpServer: server
+var wsServer = new websocket.Server({ server });
+
+wsServer.on('connection', function(ws) {
+  
+  // Set websocket to a Player object with a unique ID
+  var newID = guidGenerator();
+  ws.id = newID;
+  var newPlayer = new Player(ws);
+  players[newID] = newPlayer;
+
+  // Send player ships to client
+  newPlayer.sendShips();
+
+  ///////////////// Messages from Client to Sever////////////////
+  ws.on("message", function incoming(event) {
+    var player = players[ws.id];
+
+     // assume message is a JSON (rather low security)
+     var message = JSON.parse(event);
+    
+     if (message) {
+
+      // Message Recieved: Request for Player Ready status
+      if (message.ships !== null) {
+        let readyAccepted = player.ships.validPlacedMatch(message.ships);
+        player.respondReadyRequest(readyAccepted);
+
+        // If player is ready, try to pair player or set player to wait.
+        if (readyAccepted) {
+
+          // Start a new game between waiting player and new player
+          if (waitingPlayer) {
+            newPlayer.pairWith(waitingPlayer);
+
+            let isNewPlayerTurn = firstPlayerTurn();
+            newPlayer.turn = isNewPlayerTurn;
+            waitingPlayer.turn = !isNewPlayerTurn;
+
+            newPlayer.sendPairedMessage();
+            waitingPlayer.sendPairedMessage();
+            waitingPlayer = null;
+          
+          // if no player is waiting, this player has to wait
+          } else {
+            waitingPlayer = newPlayer;
+          }
+        }
+      }
+
+      // Message Received: Chosen Position
+     } 
+
+  });
 });
 
-// Accept any requests for connection to the websocket server
-wsServer.on('request', function(request) {
-  var connection = request.accept(null, request.origin);
-  
-  // add connection to unpaired players
-  unpairedPlayers[guidGenerator()] = connection;
-  
-  // if two players are waiting, pair them
-  var unpairedKeys = Object.keys(unpairedPlayers);
-  if (unpairedKeys.length >= 2) {
-  }
+/////////////////////////// Open the Server /////////////////////////////
 
+server.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
 });
--- battleships/public/javascripts/main.js
+++ battleships/public/javascripts/main.js
@@ -1,218 +1,261 @@
-// ships to be placed by the player. Sent to server on ready signal.
-var ships = {
-    
-    aircraftCarrier: {
-        name: "Aircraft Carrier",
-        length: 5,
-        unplaced: 1,
-        places: []
-    },
-
-    battleship: {
-        name: "Battleship",
-        length: 4,
-        unplaced: 2,
-        places: []
-    },
-
-    cruiser: {
-        name: "Cruiser",
-        length: 3,
-        unplaced: 1,
-        places: []
-    },
-
-    submarine: {
-        name: "Submarine",
-        length: 3,
-        unplaced: 2,
-        places: []
-    },
-
-    destroyer: {
-        name: "Destroyer",
-        length: 2,
-        unplaced: 2,
-        places: []
-    }
-};
 
 var main = function() {
     "use strict";
 
+    //////////////////////// Client-Side Game Data /////////////////////
+
+    // variable to save whether player is ready and waiting to start
+    var playerReady = false;
+
+    // variable to save whether the game has commenced
+    var gameCommenced = false;
+
+    var playerTurn = false;
+    
+    var ships = {};
+
+    var selectedOpponentSquare;
+
+    //////////////////// Server-Independent Processing ////////////////
+
+    // generate boards made up from divs
+    var generateBoard = function(width, height) {
+        var board = $("<div>").addClass("board");
+        board.attr("width", width);
+        board.attr("height", height);
+
+        var rowDiv;
+        var el;
+        for (let row=0; row<height; row++) {
+            rowDiv = $("<div>").addClass("row");
+            for(let column=0; column<width; column++) {
+                el = $("<div>").addClass("cell");
+                el.attr("row", row);
+                el.attr("column", column);
+                rowDiv.append(el);
+            }
+            board.append(rowDiv);
+        }
+
+        return board;
+    }
+
     // generate the boards
     $(".board-parent").append( generateBoard(10, 10) );
 
-    // allow the user to place ships and indicate when they are ready to begin
-    shipPlacement();
-};
+    // Processing to set ship positions
+    var placeShips = function() {
 
-
-// generate boards made up from divs
-var generateBoard = function(width, height) {
-    var board = $("<div>").addClass("board");
-    board.attr("width", width);
-    board.attr("height", height);
-
-    var rowDiv;
-    var el;
-    for (let row=0; row<height; row++) {
-        rowDiv = $("<div>").addClass("row");
-        for(let column=0; column<width; column++) {
-            el = $("<div>").addClass("cell");
-            el.attr("row", row);
-            el.attr("column", column);
-            rowDiv.append(el);
-        }
-        board.append(rowDiv);
-    }
-
-    return board;
-}
+        // constructs an array of cells from cell to cell + horizontal steps
+        var cellHorizontalArray = function($cell, steps) {
+            var cells = [];
     
-
-var shipPlacement = function() {
-
-    // function to check if ships are all placed
-    var shipsPlaced = function() {
-        for (let key in ships) {
-            if (ships[key].unplaced > 0) {
-                return false;
+            for (let i=0; i<steps; i++) {
+                cells.push($cell[0]);
+                $cell = $cell.next();
             }
+            return cells;
         }
-        return true;
-    }
-
-    // constructs an array of cells from cell to cell + horizontal steps
-    var cellHorizontalArray = function($cell, steps) {
-        var cells = [];
-
-        for (let i=0; i<steps; i++) {
-            cells.push($cell[0]);
-            $cell = $cell.next();
-        }
-        return cells;
-    }
-
-    // constructs an array of cells from cell to cell + vertical steps
-    var cellVerticalArray = function($cell, steps) {
-        var cells = [];
-
-        var $row = $cell.parent();
-        for (let i=0; i<steps; i++) {
-            cells.push($cell[0]);
-
-            $row = $row.next();
-            $cell = $row.children("[column='" + $cell.attr("column") + "']")
-        }
-
-        return cells;
-    }
-
-    // function to check if one or more cells in an array contain a ship.
-    var containsShip = function(cells) {
-        for (let i=0; i<cells.length; i++) {
-            if ($(cells[i]).hasClass("ship")) {
-                return true;
+    
+        // constructs an array of cells from cell to cell + vertical steps
+        var cellVerticalArray = function($cell, steps) {
+            var cells = [];
+    
+            var $row = $cell.parent();
+            for (let i=0; i<steps; i++) {
+                cells.push($cell[0]);
+    
+                $row = $row.next();
+                $cell = $row.children("[column='" + $cell.attr("column") + "']")
             }
+    
+            return cells;
         }
-        return false;
-    }
-
-    // function to set a ship at all cells in the given array.
-    var setShip = function(cells) {
-        cells.forEach( (cell) => {
-            $(cell).addClass("ship");
+    
+        // function to check if one or more cells in an array contain a ship.
+        var containsShip = function(cells) {
+            for (let i=0; i<cells.length; i++) {
+                if ($(cells[i]).hasClass("ship")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        // function to set a ship at all cells in the given array.
+        var setShip = function(cells) {
+            cells.forEach( (cell) => {
+                $(cell).addClass("ship");
+            });
+        }
+    
+        // add a ship to the player board when the board is clicked
+        $("#player-board .cell").click((event) => {
+            let $cell = $(event.currentTarget);
+            let shipKey = $("#ship-pallet input[name=ships]:checked").val();
+            let rotated = $("#ship-pallet input[name=rotate]:checked").val();
+            let ship = ships[shipKey];
+    
+            // ensure ship has been selected and ships of that type are left
+            if (ship && ship.unplaced > 0) {
+    
+                // ensure ship fits horizontally or vertically
+                var horizontalFit = !rotated && Number($cell.attr("column")) + ship.length <= $cell.closest(".board").attr("width");
+                var verticalFit = rotated && Number($cell.attr("row")) + ship.length <= $cell.closest(".board").attr("height");
+    
+                if (horizontalFit || verticalFit) {
+    
+                    var cellArray;
+                    if (horizontalFit) {
+                        cellArray = cellHorizontalArray($cell, ship.length);
+    
+                    } else {
+                        cellArray = cellVerticalArray($cell, ship.length);
+                    }
+                    
+                    if (!containsShip(cellArray)) {
+                        ship.places.push(cellArray);
+                        setShip(cellArray);
+                        ship.unplaced -= 1;
+                        $("#ship-pallet #" + shipKey + " .quantity").text(ship.unplaced);
+                    }
+                }
+            }
         });
     }
 
-    // generate ship radio buttons
-    for (let key in ships) {
-        let ship = ships[key];
-        let input = $("<input>")
-        input.attr("type", "radio").attr("name", "ships").attr("value", key);
+    placeShips();
 
-        let label = $("<label>").attr("id", key);
-        label.append(input).append(ship.name);
-        label.append(" (<span class=quantity>" + ship.unplaced + "</span>) ");
+    ////////////////////// WebSocket Initialization ////////////////////
 
-        $("#ship-pallet").append(label);
-    }
-
-    // add a ship to the player board when the board is clicked
-    $("#player-board .cell").click((event) => {
-        let $cell = $(event.currentTarget);
-        let shipKey = $("#ship-pallet input[name=ships]:checked").val();
-        let rotated = $("#ship-pallet input[name=rotate]:checked").val();
-        let ship = ships[shipKey];
-
-        // ensure ship has been selected and ships of that type are left
-        if (ship && ship.unplaced > 0) {
-
-            // ensure ship fits horizontally or vertically
-            var horizontalFit = !rotated && Number($cell.attr("column")) + ship.length <= $cell.closest(".board").attr("width");
-            var verticalFit = rotated && Number($cell.attr("row")) + ship.length <= $cell.closest(".board").attr("height");
-
-            if (horizontalFit || verticalFit) {
-
-                var cellArray;
-                if (horizontalFit) {
-                    cellArray = cellHorizontalArray($cell, ship.length);
-
-                } else {
-                    cellArray = cellVerticalArray($cell, ship.length);
-                }
-                
-                if (!containsShip(cellArray)) {
-                    ship.places.push(cellArray);
-                    setShip(cellArray);
-                    ship.unplaced -= 1;
-                    $("#ship-pallet #" + shipKey + " .quantity").text(ship.unplaced);
-                }
-            }
-        }
-    });
-
-    // hide ready button, turn off clicks for player board and change status to ready when clicked
-    $("#ready-button").click((event => {
-        if (!shipsPlaced()) {
-            window.alert("Please place all of your ships.");
-        } else {
-            $(event.currentTarget).hide();
-            $("#ship-pallet").hide();
-            $("#player-board .cell").off('click');
-            $("#message").text("Waiting for opponent to be ready...");
-            
-            // start up the web sockets!
-            gameplay();
-        }
-    }));
-}
-
-var gameplay = function() {
     // use built-in websocket for mozilla
     window.WebSocket = window.WebSocket || window.MozWebSocket
 
     // find location of server to open websocket to
     var loc = window.location, new_uri;
-    if (loc.protocol === "https:") {
-        new_uri = "wss:";
-    } else {
-        new_uri = "ws:";
-    }
-    new_uri += "//" + loc.host;
+    new_uri = "ws://" + loc.host;
     new_uri += loc.pathname + "/to/ws";
 
     // open connection to server
-    var connection = new WebSocket(new_uri);
+    var socket = new WebSocket(new_uri);
 
     // function to call when websocket connection is open.
-    connection.onopen = function() {
+    socket.onopen = function() {
         console.log("connection to server opened");
     }
 
+    ////////////////// Recieving Messages from Server ////////////////
 
-}
+    // generate ship radio buttons
+    var generateRadioButtons = function() {
+        for (let key in ships) {
+            let ship = ships[key];
+            let input = $("<input>")
+            input.attr("type", "radio").attr("name", "ships").attr("value", key);
 
+            let label = $("<label>").attr("id", key);
+            label.append(input).append(ship.name);
+            label.append(" (<span class=quantity>" + ship.unplaced + "</span>) ");
+
+            $("#ship-pallet").append(label);
+        }
+    }
+
+    // function to set everything to ready-to-start status
+    var setPlayerReady = function() {
+        $("#ready-button").hide();
+        $("#ship-pallet").hide();
+        $("#player-board .cell").off('click');
+        $("#message p").text("Waiting for another player...");
+        playerReady = true;
+    }
+
+    // function to inform the player if the ready request was denied
+    var onReadyRequestDenied = function() {
+        window.alert("Have you placed all your ships?");
+    }
+
+    // function to kick off game
+    var startGame = function(myTurn) {
+        gameCommenced = true;
+        playerTurn = myTurn;
+        
+        if (playerTurn) {
+            $("#message p").text("Choose an opponent square to attack");
+            enableOpponentSquare();
+        } else {
+            $("#message p").text("Opponent's turn...");
+        }
+
+    }
+
+    // Server Messages //
+    socket.onmessage = function(event) {
+
+        // assume that message is a JSON string
+        var message = JSON.parse(event.data);
+        console.log("Message:", message);
+
+        if (message) {
+            
+            // Receive Message: Player Ships
+            if (message.ships != undefined) {
+                console.log("Received ships");
+                ships = message.ships;
+                generateRadioButtons();
+            }
+
+            // Receive Message: Ready Request Response
+            else if (message.readyAccepted != undefined) {
+                console.log("Accepted: ", message.readyAccepted);
+                if (message.readyAccepted) {
+                    setPlayerReady();
+                } else {
+                    onReadyRequestDenied();
+                }
+            }
+
+            // Receive Message: Found Opponent
+            else if (message.foundOpponent != undefined && message.foundOpponent) {
+                startGame(message.yourTurn);
+            }
+
+        }
+        // Message: Found opponent
+
+        // Message: your go
+
+        // Message: Sunk opponent ship
+        // Message: Hit opponent ship
+        // Message: Missed opponent ship
+
+        // Message: Sunk your ship
+        // Message: Hit your ship
+        // Message: Missed your ship
+
+        // Message: Player has won
+        // Message: Player has lost
+    }
+
+    //////////////////// Sending Messages to Server ///////////////////
+
+    // send request to server to set player as ready to play
+    $("#ready-button").click((event => {
+        socket.send( JSON.stringify({'ships': ships}) );
+    }));
+
+    function enableOpponentSquare() {
+        $("#opponent-board .cell").click((event) => {
+            var $cell = $(event.currentTarget);
+            //if (playerTurn) {
+                window.alert(`Attacked Opponent Square (${$cell.attr("row")}, ${$cell.attr("column")})`);
+            //}
+        });
+    }
+
+    function disableOpponentSquare() {
+        $("#opponent-board .cell").off('click');
+    }
+};
 
 $(document).ready(main);
